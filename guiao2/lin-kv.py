8bf9a68218c68c72266d501e742f2320001c69ea
JoaoSG56 <pg47329@alunos.uminho.pt>
2022-03-14 16:04:49 +0000

    + error 11

--- guiao2/lin-kv.py
+++ guiao2/lin-kv.py
@@ -12,27 +12,28 @@ from ms import send, receiveAll, reply, exitOnError
 logging.getLogger().setLevel(logging.DEBUG)
 executor=ThreadPoolExecutor(max_workers=1)
 
-def handleRead(msg):
-    v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
-
-    if v[1] is None:
-    # Se o valor do nodo que tem maior versao for none então não existe esse valor
-        send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',code=20,text='not found')
-    else:
-        # Se tiver valor, então responde com read_ok para o cliente
-        send(node_id,requests[str(msg.body.request_id)]['src'],type = 'read_ok',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],value=v[1])
 
 
 def handle(msg):
     # State
-    global node_id, node_ids,version, responses,requests,request_id,locked, locked_requests, dic
+    global node_id, node_ids,version, responses,requests,request_id,locked, dic
+    
+    # def handleRead(msg):
+    #     v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
+
+    #     if v[1] is None:
+    #     # Se o valor do nodo que tem maior versao for none então não existe esse valor
+    #         send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',code=20,text='not found')
+    #     else:
+    #         # Se tiver valor, então responde com read_ok para o cliente
+    #         send(node_id,requests[str(msg.body.request_id)]['src'],type = 'read_ok',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],value=v[1])
+
 
     # Message handlers
     if msg.body.type == 'init':
         node_id = msg.body.node_id
         node_ids = msg.body.node_ids
 
-        locked_requests = []
         locked = None
 
         # key: (timestamp,value)
@@ -84,7 +85,6 @@ def handle(msg):
         requests[str(msg.body.request_id)]["responses"].append(msg.body.value)
         if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
             # Já recebeu as respostas de todos os nodos
-
             #handleRead(msg)
             # Calculo do timestamp maior:
             v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
@@ -97,17 +97,38 @@ def handle(msg):
                 send(node_id,requests[str(msg.body.request_id)]['src'],type = 'read_ok',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],value=v[1])
     elif msg.body.type == 'error':
         # insere par de resposta (timestamp,None) no campo de respostas de um dado request do cliente (dado pelo request_id)
-        requests[str(msg.body.request_id)]["responses"].append((-1,None))  
+        if msg.body.code == 11:
+            # -2 caso seja code 11
+            requests[str(msg.body.request_id)]["responses"].append((-2,None))
+        else:
+            requests[str(msg.body.request_id)]["responses"].append((-1,None))
+                
+        # if msg.body.code == 11 and requests[str(msg.body.request_id)]["error"] != True:
+        #     requests[str(msg.body.request_id)]["error"]=True
+        #     for s in requests[str(msg.body.request_id)]["locked_servers"]:
+        #         if s != node_id:
+        #             send(node_id,s,type="unlock")
+        #     send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=11,text='not available')
+
         if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
             # Já recebeu as respostas de todos os nodos
-            if requests[str(msg.body.request_id)]["type"] == "read":
+            flag = False
+            for r in requests[str(msg.body.request_id)]["responses"]:
+                if r[0] == -2:
+                    flag = True
+            if flag:
+                for s in requests[str(msg.body.request_id)]["quorums"]:
+                    send(node_id,s,type="unlock",request_id=msg.body.request_id)
+                send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=11,text='not available')
+   
+            elif requests[str(msg.body.request_id)]["type"] == "read":
                 # type == read
 
                 #handleRead(msg)
                 v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
 
                 for s in requests[str(msg.body.request_id)]["quorums"]:
-                    send(node_id,s,type="unlock")
+                    send(node_id,s,type="unlock",request_id=msg.body.request_id)
 
                 if v[1] is None:
                     send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=20,text='not found')
@@ -121,18 +142,18 @@ def handle(msg):
                     # Caso o valor seja None, dá reply com not found e envia unlocks aos servidores
                     send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="20",text="not found")
                     for s in requests[str(msg.body.request_id)]["quorums"]:
-                        send(node_id,s,type="unlock")
+                        send(node_id,s,type="unlock",request_id=msg.body.request_id)
 
                 elif v[1] != requests[str(msg.body.request_id)]["from"]:
                     # Caso o valor seja diferente do que esta no from, da reply com not equal e envia unlocks aos servidores
                     send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="22",text="not equal")
                     for s in requests[str(msg.body.request_id)]["quorums"]:
-                        send(node_id,s,type="unlock")
+                        send(node_id,s,type="unlock",request_id=msg.body.request_id)
                 else:
                     # escreve nos quorums e dá unlock
                     for s in requests[str(msg.body.request_id)]["quorums"]:
                         send(node_id,s,type="write",key=requests[str(msg.body.request_id)]["key"],value=requests[str(msg.body.request_id)]["to"],timestamp=v[0]+1)
-                        send(node_id,s,type="unlock")
+                        #send(node_id,s,type="unlock")
                     send(node_id,requests[str(msg.body.request_id)]["src"],type="cas_ok")
 
         
@@ -147,7 +168,7 @@ def handle(msg):
             quorum = random.sample(node_ids,w)
             logging.info("QUORUMS:")
             logging.info(quorum)
-            requests[str(request_id)] = {"src":msg.src,"msg_id":msg.body.msg_id,"type":"write","key":msg.body.key,"value":msg.body.value,"responses":[],"quorums":quorum}
+            requests[str(request_id)] = {"src":msg.src,"msg_id":msg.body.msg_id,"type":"write","key":msg.body.key,"value":msg.body.value,"responses":[],"quorums":quorum,"error":False}
 
             for s in quorum:
                 send(node_id,s,type = 'lockread',request_id=request_id,key=msg.body.key)
@@ -162,51 +183,63 @@ def handle(msg):
                     dic[msg.body.key] = (msg.body.timestamp,msg.body.value)
                     if msg.body.timestamp > version:
                         version = msg.body.timestamp
+                locked = None
 
     elif msg.body.type == 'lockread_ok':
         # caso seja obtido o lock
         requests[str(msg.body.request_id)]["responses"].append(msg.body.value)
-        if requests[str(msg.body.request_id)]["type"] == "write":
-            # insere o timestamp nas respostas e quando as obtiver todas, escreve nos nodos e responde ao cliente
-            if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
-                v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
+        if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
+            flag = False
+            for r in requests[str(msg.body.request_id)]["responses"]:
+                if r[0] == -2:
+                    flag = True
+            if flag:
                 for s in requests[str(msg.body.request_id)]["quorums"]:
-                    send(node_id,s,type="write",key=requests[str(msg.body.request_id)]["key"],value=requests[str(msg.body.request_id)]["value"],timestamp=v[0]+1)
-                    send(node_id,s,type="unlock")
-                send(node_id,requests[str(msg.body.request_id)]["src"],in_reply_to=requests[str(msg.body.request_id)]["msg_id"],type="write_ok")
-        elif requests[str(msg.body.request_id)]["type"] == "cas":
-            # insere o (timestamp,value) nas respostas e quando as obtiver todas, escreve nos nodos, dá unlock e responde ao cliente
-            if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
-                v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
-                if v[1] is None:
-                    send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="20",text="not found")
+                    send(node_id,s,type="unlock",request_id=msg.body.request_id)
+                send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=11,text='not available')
+
+
+            elif requests[str(msg.body.request_id)]["type"] == "write":
+                # insere o timestamp nas respostas e quando as obtiver todas, escreve nos nodos e responde ao cliente
+                if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
+                    v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
                     for s in requests[str(msg.body.request_id)]["quorums"]:
-                        send(node_id,s,type="unlock")
+                        send(node_id,s,type="write",key=requests[str(msg.body.request_id)]["key"],value=requests[str(msg.body.request_id)]["value"],timestamp=v[0]+1)
+                        #send(node_id,s,type="unlock")
+                    send(node_id,requests[str(msg.body.request_id)]["src"],in_reply_to=requests[str(msg.body.request_id)]["msg_id"],type="write_ok")
+            elif requests[str(msg.body.request_id)]["type"] == "cas":
+                # insere o (timestamp,value) nas respostas e quando as obtiver todas, escreve nos nodos, dá unlock e responde ao cliente
+                if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
+                    v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
+                    if v[1] is None:
+                        send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="20",text="not found")
+                        for s in requests[str(msg.body.request_id)]["quorums"]:
+                            send(node_id,s,type="unlock",request_id=msg.body.request_id)
 
-                elif v[1] != requests[str(msg.body.request_id)]["from"]:
-                    send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="22",text="not equal")
-                    for s in requests[str(msg.body.request_id)]["quorums"]:
-                        send(node_id,s,type="unlock")
-                else:
-                 
-                    for s in requests[str(msg.body.request_id)]["quorums"]:
-                        send(node_id,s,type="write",key=requests[str(msg.body.request_id)]["key"],value=requests[str(msg.body.request_id)]["to"],timestamp=v[0]+1)
-                        send(node_id,s,type="unlock")
-                    send(node_id,requests[str(msg.body.request_id)]["src"],in_reply_to=requests[str(msg.body.request_id)]["msg_id"],type="cas_ok")
-        elif requests[str(msg.body.request_id)]["type"] == "read":
-            # type == read
-            if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
-                v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
-
-                for s in requests[str(msg.body.request_id)]["quorums"]:
-                    send(node_id,s,type="unlock")
-
-                if v[1] is None:
-                    send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=20,text='not found')
-                else:
-                    send(node_id,requests[str(msg.body.request_id)]['src'],type = 'read_ok',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],value=v[1])
-
+                    elif v[1] != requests[str(msg.body.request_id)]["from"]:
+                        send(node_id,requests[str(msg.body.request_id)]["src"],type="error",in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code="22",text="not equal")
+                        for s in requests[str(msg.body.request_id)]["quorums"]:
+                            send(node_id,s,type="unlock",request_id=msg.body.request_id)
+                    else:
                     
+                        for s in requests[str(msg.body.request_id)]["quorums"]:
+                            send(node_id,s,type="write",key=requests[str(msg.body.request_id)]["key"],value=requests[str(msg.body.request_id)]["to"],timestamp=v[0]+1)
+                            #send(node_id,s,type="unlock")
+                        send(node_id,requests[str(msg.body.request_id)]["src"],in_reply_to=requests[str(msg.body.request_id)]["msg_id"],type="cas_ok")
+            elif requests[str(msg.body.request_id)]["type"] == "read":
+                # type == read
+                if len(requests[str(msg.body.request_id)]["responses"]) == math.ceil((len(node_ids)+1)/2):
+                    v = max(requests[str(msg.body.request_id)]["responses"], key = lambda x : x[0])
+
+                    for s in requests[str(msg.body.request_id)]["quorums"]:
+                        send(node_id,s,type="unlock",request_id=msg.body.request_id)
+
+                    if v[1] is None:
+                        send(node_id,requests[str(msg.body.request_id)]['src'],type = 'error',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],code=20,text='not found')
+                    else:
+                        send(node_id,requests[str(msg.body.request_id)]['src'],type = 'read_ok',in_reply_to=requests[str(msg.body.request_id)]["msg_id"],value=v[1])
+
+                
     elif msg.body.type == 'lockread':
         # caso um nodo queira um lock e ler o valor de uma key
         if not locked:
@@ -219,22 +252,23 @@ def handle(msg):
                 reply(msg,type="lockread_ok",request_id=locked[1],value=(-1,None))
         else:
             # insere numa queue de locks
-            locked_requests.append((msg.src,msg.body.request_id,msg.body.key))
-        
+            #locked_requests.append((msg.src,msg.body.request_id,msg.body.key))
+            reply(msg,type='error',code=11,request_id=msg.body.request_id)
+
     elif msg.body.type == 'unlock':
         # unlock vindo de um nodo
-        if locked[0] == msg.src:
+        if locked is not None and locked[0] == msg.src and locked[1] == msg.body.request_id:
             # se o unlock for feito pelo nodo que o possui , dá unlock e responde com unlock_ok
             locked = None
-        reply(msg,type="unlock_ok")
-        if len(locked_requests) > 0:
-            # se houver pedidos de lock na queue, dá pop e responde conforme o pedido
-            locked = locked_requests.pop(0)
-            # falta enviar outros campos + adicionar campo key no locked
-            if locked[2] in dic:
-                send(node_id,locked[0],type='lockread_ok',request_id=locked[1],value=dic[locked[2]])
-            else:
-                send(node_id,locked[0],type='lockread_ok',request_id=locked[1],value=(-1,None))
+            reply(msg,type="unlock_ok",request_id=msg.body.request_id)
+        # if len(locked_requests) > 0:
+        #     # se houver pedidos de lock na queue, dá pop e responde conforme o pedido
+        #     locked = locked_requests.pop(0)
+        #     # falta enviar outros campos + adicionar campo key no locked
+        #     if locked[2] in dic:
+        #         send(node_id,locked[0],type='lockread_ok',request_id=locked[1],value=dic[locked[2]])
+        #     else:
+        #         send(node_id,locked[0],type='lockread_ok',request_id=locked[1],value=(-1,None))
     
 
     elif msg.body.type == 'cas':
@@ -243,7 +277,7 @@ def handle(msg):
         quorum = random.sample(node_ids,w)
 
         # cria entrada na tabela de requests
-        requests[str(request_id)] = {"src":msg.src,"msg_id":msg.body.msg_id,"type":"cas","key":msg.body.key,"from":getattr(msg.body,'from'),"to":msg.body.to,"responses":[],"quorums":quorum}
+        requests[str(request_id)] = {"src":msg.src,"msg_id":msg.body.msg_id,"type":"cas","key":msg.body.key,"from":getattr(msg.body,'from'),"to":msg.body.to,"responses":[],"quorums":quorum,"error":False}
 
         # envia lock and read ao quorum que depois é tratado no 'if type==lockread_ok'
         for s in quorum:
